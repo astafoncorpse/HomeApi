8688e31d4714c8e95dedb1b5d1972df789b953bd
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-07-22 18:34:58 +0300

    Добавьте файлы проекта.

---
+++ HomeApi/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HomeApi.Configuration;
+using HomeApi.Contracts;
+using HomeApi.Contracts.Devices;
+
+namespace HomeApi
+{
+    /// <summary>
+    /// Настройки маппинга всех сущностей приложения
+    /// </summary>
+    public class MappingProfile : Profile
+    {
+        /// <summary>
+        /// В конструкторе настроим соответствие сущностей при маппинге
+        /// </summary>
+        public MappingProfile()
+        {
+            CreateMap<AddDeviceRequest, AddDeviceRequest>();
+            CreateMap<Address, AddressInfo>();
+            CreateMap<HomeOptions, InfoResponse>()
+                .ForMember(m => m.AddressInfo,
+                    opt => opt.MapFrom(src => src.Address));
+           
+        }
+    }
+}
---
+++ HomeApi/Startup.cs
@@ -0,0 +1,84 @@
+using HomeApi.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HomeApi
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Загрузка конфигурации из файла Json
+        /// </summary>
+        public IConfiguration Configuration
+        { get; } = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json")
+          .AddJsonFile("HomeOptions.json")
+            .AddJsonFile("AddDiviceRequest.json")
+          .Build();
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Добавляем новый сервис
+            services.Configure<HomeOptions>(Configuration);
+            // Добавляем новый сервис
+            services.Configure<HomeOptions>(Configuration);
+            services.Configure<HomeOptions>(opt =>
+            {
+                opt.Area = 120;
+            });
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HomeApi", Version = "v1" });
+            });
+            // Подключаем автомаппинг
+            var assembly = Assembly.GetAssembly(typeof(MappingProfile));
+            services.AddAutoMapper(assembly);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HomeApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
