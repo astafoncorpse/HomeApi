c2a779f4b280e14b7fb8f53e9b8cf32552a7c1d9
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-07-22 18:39:07 +0300

    34.5

--- HomeApi/Controllers/DevicesController.cs
+++ HomeApi/Controllers/DevicesController.cs
@@ -39,11 +39,17 @@ namespace HomeApi.Controllers
         [HttpPost]
         [Route("Add")]
         public IActionResult Add(
-          [FromBody] // Атрибут, указывающий, откуда брать значение объекта
-              AddDeviceRequest request // Объект запроса
+           [FromBody] // Атрибут, указывающий, откуда биндить значение объекта
+         AddDeviceRequest request // Объект запроса
         )
-           
         {
+            if (request.CurrentVolts < 120)
+            {
+                // Добавляем для клиента информативную ошибку
+                ModelState.AddModelError("currentVolts", "Устройства с напряжением меньше 120 вольт не поддерживаются!");
+                return BadRequest(ModelState);
+            }
+
             return StatusCode(200, $"Устройство {request.Name} добавлено!");
         }
     }
